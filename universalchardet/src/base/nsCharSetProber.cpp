afaa590f33df5e73d1fc8a7d1fa61ede82161d43
C.W. Betts <computers57@hotmail.com>
2016-05-20 10:45:20 -0600

    Quiet 64-to-32 bit conversion warning.

--- universalchardet/src/base/nsCharSetProber.cpp
+++ universalchardet/src/base/nsCharSetProber.cpp
@@ -41,7 +41,7 @@ bool nsCharSetProber::FilterWithoutEnglishLetters(const char* aBuf, uint32_t aLe
   if (meetMSB && curPtr > prevPtr) 
     while (prevPtr < curPtr) *newptr++ = *prevPtr++;  
 
-  newLen = newptr - *newBuf;
+  newLen = uint32_t(newptr - *newBuf);
 
   return true;
 }
@@ -86,7 +86,7 @@ bool nsCharSetProber::FilterWithEnglishLetters(const char* aBuf, uint32_t aLen, 
     while (prevPtr < curPtr)
       *newptr++ = *prevPtr++;  
 
-  newLen = newptr - *newBuf;
+  newLen = uint32_t(newptr - *newBuf);
 
   return true;
 }
